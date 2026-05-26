b84f702c73440392c87dc674bc9504a0a68c2e9c
Maria <mia.gotcha@proton.me>
2026-05-26 14:12:37 +0200

    extend error handling in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import { SWRConfig } from "swr";
 import GlobalStyle from "../styles";
 
-const fetcher = (url) => fetch(url).then((response) => response.json());
+const fetcher = async (resource, init) => {
+  const result = await fetch(resource, init);
+
+  if (!result.ok) {
+    const error = new Error("An error occurred while fetching the data.");
+    error.info = await result.json();
+    error.status = result.status;
+    throw error;
+  }
+
+  return result.json();
+};
 
 export default function App({ Component, pageProps }) {
   return (
