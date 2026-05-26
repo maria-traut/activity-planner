2fe4ab2fdc248c569c45e73e9e0c94dc2f7d3813
Maria <mia.gotcha@proton.me>
2026-05-26 12:52:13 +0200

    import swrconfig, use it on homepage, add handling for loading and error states

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,36 @@
+import useSWR from "swr";
+
 export default function HomePage() {
+  const { data, isLoading, error } = useSWR("/api/activities");
+
+  if (isLoading)
+    return (
+      <div>
+        <h1>Activity Planner</h1>
+        <p>Loading activities...</p>
+      </div>
+    );
+
+  if (error)
+    return (
+      <div>
+        <h1>Activity Planner</h1>
+        <p>An error occured while fetching data.</p>
+      </div>
+    );
+
+  if (!data)
+    return (
+      <div>
+        <h1>Activity Planner</h1>
+        <p>There is no activities yet.</p>
+      </div>
+    );
+
   return (
     <div>
-      <h1>Hello from Activity Planner</h1>
+      <h1>Activity Planner</h1>
+      {console.log(data)}
     </div>
   );
 }
