79009c79c2173ee068207d3e155cc72e028e1c9f
Marco <m.hebisch@outlook.com>
2026-05-26 10:42:25 +0200

    added mongoose connection to database and Schema for activity and category collections

---
+++ db/models/Activity.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import "./Category";
+
+const { Schema } = mongoose;
+
+const activitySchema = new Schema({
+  title: { type: String, required: true, minlength: 3 },
+  imageUrl: { type: String, required: true, default: `/placeholder.jpg` },
+  categories: { type: [Schema.Types.ObjectId], ref: "Category" },
+  description: { type: String },
+  area: { type: String },
+  country: { type: String },
+});
+
+const Activity =
+  mongoose.models.Activity || mongoose.model("Activity", activitySchema);
+
+export default Activity;
