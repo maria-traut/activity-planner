f9ee5bb21b94d96dc69993463d148fcfa94dceaa
Maria <mia.gotcha@proton.me>
2026-05-29 16:49:20 +0200

    import useState, conditionally render form, create Apply and Reset button

--- components/SortButton/index.js
+++ components/SortButton/index.js
@@ -1,20 +1,45 @@
+import { useState } from "react";
+import styled from "styled-components";
+
 export default function SortButton() {
+  const [isSortActivityMode, setIsSortActivityMode] = useState(false);
+
   return (
-    <div>
-      <div>
-        <label htmlFor="sort-title">Title </label>
-        <select id="sort-title">
-          <option value="az">A to Z</option>
-          <option value="za">Z to A</option>
-        </select>
-      </div>
-      <div>
-        <label htmlFor="sort-date">Date </label>
-        <select id="sort-date">
-          <option value="newest">New to Old</option>
-          <option value="oldest">Old to New</option>
-        </select>
-      </div>
-    </div>
+    <>
+      <button
+        type="button"
+        onClick={() => setIsSortActivityMode(!isSortActivityMode)}
+      >
+        Sort{isSortActivityMode ? " ▲" : " ▼"}
+      </button>
+      {isSortActivityMode && (
+        <form>
+          <fieldset>
+            <StyledSortWrapper>
+              <div>
+                <label htmlFor="sort-title">Title </label>
+                <select id="sort-title">
+                  <option value="az">A to Z</option>
+                  <option value="za">Z to A</option>
+                </select>
+              </div>
+              <div>
+                <label htmlFor="sort-date">Date </label>
+                <select id="sort-date">
+                  <option value="newest">New to Old</option>
+                  <option value="oldest">Old to New</option>
+                </select>
+              </div>
+            </StyledSortWrapper>
+            <button type="submit">Apply</button>
+            <button type="button">Reset</button>
+          </fieldset>
+        </form>
+      )}
+    </>
   );
 }
+
+const StyledSortWrapper = styled.div`
+  display: flex;
+`;
