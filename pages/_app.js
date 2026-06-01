63edad9809b8407c07400299d0e52ff7dc9ccc72
Artemiy <88054498+ArtemiyShokin@users.noreply.github.com>
2026-06-01 15:46:01 +0200

    Update pages/_app.js
    Co-authored-by: FelineHuhn <112863528+FelineHuhn@users.noreply.github.com>

--- pages/_app.js
+++ pages/_app.js
@@ -31,7 +31,7 @@ export default function App({ Component, pageProps }) {
                 );
             }
 
-            return [...prevBookmarkedActivities, id];
+            return [...prevBookmarkedActivityIds, id];
         });
     }
     function handleBookmarkedActivityIdsDelete(id) {
