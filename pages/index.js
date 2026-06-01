9f51b069bf222e4905b9c0b18c76a2c3dad5a734
Maria <mia.gotcha@proton.me>
2026-06-01 15:17:01 +0200

    solve merge conflict

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import useSWR from "swr";
 import { useState, useEffect } from "react";
 import ActivityForm from "@/components/ActivityForm";
 import ActivityList from "@/components/ActivityList";
-
 import SortButton from "@/components/SortButton";
 import Head from "next/head";
 import Header from "@/components/Header";
@@ -58,11 +57,6 @@ export default function HomePage() {
           })
         : [];
 
-    function handleActivitySort(order) {
-        console.log(order);
-        setActivitySortOrder(order);
-    }
-
     useEffect(() => {
         const successMessageTimer = setTimeout(() => {
             if (activityFormStatus.type === "success") {
@@ -70,11 +64,16 @@ export default function HomePage() {
                     type: "",
                     message: "",
                 });
+                setIsCreateActivityMode(false);
             }
         }, 3000);
 
         return () => clearTimeout(successMessageTimer);
     }, [activityFormStatus]);
+
+    function handleActivitySort(order) {
+        setActivitySortOrder(order);
+    }
 
     async function handleActivityCreate(event) {
         event.preventDefault();
@@ -93,86 +92,22 @@ export default function HomePage() {
             body: JSON.stringify(activityData),
         });
 
-        useEffect(() => {
-            const successMessageTimer = setTimeout(() => {
-                if (activityFormStatus.type === "success") {
-                    setActivityFormStatus({
-                        type: "",
-                        message: "",
-                    });
-                    setIsCreateActivityMode(false);
-                }
-            }, 3000);
-
-            return () => clearTimeout(successMessageTimer);
-        }, [activityFormStatus]);
-
-        async function handleActivityCreate(event) {
-            event.preventDefault();
-
-            const formData = new FormData(event.target);
-            const activityData = {
-                ...Object.fromEntries(formData),
-                categories: formData.getAll("categories"),
-            };
-
-            const response = await fetch(`/api/activities`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(activityData),
+        if (response.ok) {
+            mutate();
+            event.target.reset();
+            setActivityFormStatus({
+                type: "success",
+                message: "Activity has successfully been created!",
             });
-
-            if (response.ok) {
-                mutate();
-                event.target.reset();
-                setActivityFormStatus({
-                    type: "success",
-                    message: "Activity has successfully been created!",
-                });
-            } else {
-                setActivityFormStatus({
-                    type: "error",
-                    message: "Form could not be sent. Please try again.",
-                });
-            }
+        } else {
+            setActivityFormStatus({
+                type: "error",
+                message: "Form could not be sent. Please try again.",
+            });
         }
+    }
 
-        if (isLoading) {
-            return (
-                <>
-                    <Head>
-                        <title>Home | Activity Planner</title>
-                    </Head>
-                    <Header />
-                    <main>
-                        <StyledStatusMessageWrap>
-                            <p>Loading activities...</p>
-                        </StyledStatusMessageWrap>
-                    </main>
-                </>
-            );
-        }
-
-        if (!activities || error) {
-            return (
-                <>
-                    <Head>
-                        <title>Home | Activity Planner</title>
-                    </Head>
-                    <Header />
-                    <main>
-                        <StyledStatusMessageWrap>
-                            <p>
-                                An error occured while fetching the activities.
-                            </p>
-                        </StyledStatusMessageWrap>
-                    </main>
-                </>
-            );
-        }
-
+    if (isLoading) {
         return (
             <>
                 <Head>
@@ -180,35 +115,9 @@ export default function HomePage() {
                 </Head>
                 <Header />
                 <main>
-                    {!isCreateActivityMode && (
-                        <StyledToolbarWrap>
-                            <StyledToolbar>
-                                <StyledButton
-                                    onClick={() => {
-                                        setIsCreateActivityMode(
-                                            !isCreateActivityMode
-                                        );
-                                        setActivityFormStatus({
-                                            type: "",
-                                            message: "",
-                                        });
-                                    }}
-                                >
-                                    Create Activity
-                                </StyledButton>
-                            </StyledToolbar>
-                        </StyledToolbarWrap>
-                    )}
-                    {isCreateActivityMode && (
-                        <ActivityForm
-                            onSubmit={handleActivityCreate}
-                            status={activityFormStatus}
-                            heading="Add Activity"
-                            setIsCreateActivityMode={setIsCreateActivityMode}
-                            isCreateActivityMode={isCreateActivityMode}
-                        />
-                    )}
-                    <ActivityList activities={activities} />
+                    <StyledStatusMessageWrap>
+                        <p>Loading activities...</p>
+                    </StyledStatusMessageWrap>
                 </main>
             </>
         );
@@ -216,23 +125,58 @@ export default function HomePage() {
 
     if (!activities || error) {
         return (
-            <div>
-                <StyledHeading>Activity Planner</StyledHeading>
-                <p>An error occured while fetching the activities.</p>
-            </div>
+            <>
+                <Head>
+                    <title>Home | Activity Planner</title>
+                </Head>
+                <Header />
+                <main>
+                    <StyledStatusMessageWrap>
+                        <p>An error occured while fetching the activities.</p>
+                    </StyledStatusMessageWrap>
+                </main>
+            </>
         );
     }
 
     return (
-        <div>
-            <StyledHeading>Activity Planner</StyledHeading>
-            <ActivityForm
-                onSubmit={handleActivityCreate}
-                status={activityFormStatus}
-                heading="Add Activity"
-            />
-            <SortButton onActivitySort={handleActivitySort} />
-            <ActivityList activities={sortedActivities} />
-        </div>
+        <>
+            <Head>
+                <title>Home | Activity Planner</title>
+            </Head>
+            <Header />
+            <main>
+                {!isCreateActivityMode && (
+                    <StyledToolbarWrap>
+                        <StyledToolbar>
+                            <StyledButton
+                                onClick={() => {
+                                    setIsCreateActivityMode(
+                                        !isCreateActivityMode
+                                    );
+                                    setActivityFormStatus({
+                                        type: "",
+                                        message: "",
+                                    });
+                                }}
+                            >
+                                Create Activity
+                            </StyledButton>
+                        </StyledToolbar>
+                    </StyledToolbarWrap>
+                )}
+                {isCreateActivityMode && (
+                    <ActivityForm
+                        onSubmit={handleActivityCreate}
+                        status={activityFormStatus}
+                        heading="Add Activity"
+                        setIsCreateActivityMode={setIsCreateActivityMode}
+                        isCreateActivityMode={isCreateActivityMode}
+                    />
+                )}
+                <SortButton onActivitySort={handleActivitySort} />
+                <ActivityList activities={sortedActivities} />
+            </main>
+        </>
     );
 }
