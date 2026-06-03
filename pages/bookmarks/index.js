0c06529450bebb6c43a3a50f477fc9c098299be7
Maria <mia.gotcha@proton.me>
2026-06-03 14:50:13 +0200

    implement use state, sortedActivities variable and handle function

--- pages/bookmarks/index.js
+++ pages/bookmarks/index.js
@@ -1,4 +1,5 @@
 import useSWR from "swr";
+import { useState } from "react";
 import Head from "next/head";
 import Header from "@/components/Header";
 import BackButton from "@/components/BackButton";
@@ -18,6 +19,46 @@ export default function Bookmarks({
     const bookmarkedActivities = activities?.filter((activity) =>
         bookmarkedActivityIds.includes(activity._id)
     );
+
+    const [activitySortOrder, setActivitySortOrder] = useState("newest");
+
+    const sortedBookmarkedActivities = bookmarkedActivities
+        ? [...bookmarkedActivities].sort((a, b) => {
+              if (activitySortOrder === "az") {
+                  if (a.title.toUpperCase() > b.title.toUpperCase()) return 1;
+                  if (a.title.toUpperCase() < b.title.toUpperCase()) return -1;
+                  return 0;
+              }
+              if (activitySortOrder === "za") {
+                  if (b.title.toUpperCase() > a.title.toUpperCase()) return 1;
+                  if (b.title.toUpperCase() < a.title.toUpperCase()) return -1;
+                  return 0;
+              }
+              if (activitySortOrder === "lastModified") {
+                  return (
+                      new Date(b.updatedAt).getTime() -
+                      new Date(a.updatedAt).getTime()
+                  );
+              }
+              if (activitySortOrder === "newest") {
+                  return (
+                      new Date(b.createdAt).getTime() -
+                      new Date(a.createdAt).getTime()
+                  );
+              }
+              if (activitySortOrder === "oldest") {
+                  return (
+                      new Date(a.createdAt).getTime() -
+                      new Date(b.createdAt).getTime()
+                  );
+              }
+              return 0;
+          })
+        : [];
+
+    function handleActivitySort(order) {
+        setActivitySortOrder(order);
+    }
 
     if (isLoading) {
         return (
@@ -71,9 +112,9 @@ export default function Bookmarks({
                 </StyledToolbarWrap>
                 {bookmarkedActivities.length > 0 ? (
                     <>
-                        <SortButton />
+                        <SortButton onActivitySort={handleActivitySort} />
                         <ActivityList
-                            activities={bookmarkedActivities}
+                            activities={sortedBookmarkedActivities}
                             handleBookmarkToggle={handleBookmarkToggle}
                             bookmarkedActivityIds={bookmarkedActivityIds}
                         />
