1e143dc124592824bfa2e3ddb1d6c8359ced4213
Maria <mia.gotcha@proton.me>
2026-06-04 16:20:46 +0200

    style icon and title as centered splash screen

--- pages/loading.js
+++ pages/loading.js
@@ -1,10 +1,28 @@
-import BeeIcon from "@/components/BeeIcon";
+import NewBeeIcon from "@/components/BeeIcon/newbee";
+import styled from "styled-components";
 
 export default function Loading() {
     return (
-        <>
-            <BeeIcon />
-            <p>ActiviBee</p>
-        </>
+        <StyledSplashContainer>
+            <StyledBeeIcon width={100} height={100} />
+            <StyledSplashText>ActiviBee</StyledSplashText>
+        </StyledSplashContainer>
     );
 }
+
+const StyledSplashContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+`;
+
+const StyledBeeIcon = styled(NewBeeIcon)`
+    width: 150px;
+    height: 150px;
+`;
+
+const StyledSplashText = styled.p`
+    font-size: 2rem;
+`;
