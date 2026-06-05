3be14f685971c069ed6e4c250bc38372b056c9a5
Maria <mia.gotcha@proton.me>
2026-06-05 12:53:20 +0200

    create Pages/map.js, import useSWR and add fetcher function

--- pages/map.js
+++ pages/map.js
@@ -1,3 +1,14 @@
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((response) => response.json());
+
 export default function Map() {
+    const { data } = useSWR("https://restcountries.com/v3.1/alpha/PE", fetcher);
+    console.log(data);
+
+    const coords = data?.[0]?.latlng;
+
+    console.log(coords);
+
     return <p>My Activity Map</p>;
 }
